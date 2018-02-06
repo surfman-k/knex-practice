a39da5cdb9d9b51e9c3083f8ebd6c4381bcb3c25
Konrad <konrad.kipczak@gmail.com>
2018-02-06 15:28:43 +0000

    fixed script so that it can be searched from node by last_name as an argument

--- test_script.js
+++ test_script.js
@@ -10,15 +10,17 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
+console.log(client.database);
+
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query("SELECT * FROM famous_people WHERE last_name = $1",[process.argv[2]], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
+    console.log(result.rows); //output: 1
     client.end();
   });
 });
